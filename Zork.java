f3e16f83248a6a9a95394223432a957bb4d7f1f4
August <52710638+K-August@users.noreply.github.com>
2019-09-10 09:38:42 -0500

    Initial Commit from School
    All of the work I have done up to this point from school.

--- Zork.java
+++ Zork.java
@@ -1,25 +1,106 @@
+import java.util.Scanner;
+
 public class Zork
 {
-  static class PlayerPostion
-  {
-    private int x;
-    private int y;
-    private String position;
-    
-    PlayerPosition()
+    static class PlayerPosition
     {
-      x = 0;
-      y = 0;
+        private int x;
+        private int y;
+        private String position;
+
+        PlayerPosition()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        private String UpdateCoord()
+        {
+            return position = String.format("(%s, %s)", this.x, this.y);
+        }
     }
-    
-    private void GetPosition()
+
+    private static PlayerPosition pos = new PlayerPosition();
+
+    private static boolean wantsToPlay = true;
+
+    private static int[] gameBoard =
+            {
+                    -2, -1, 0, 1, 2
+            };
+
+    public static void main(String[] args)
     {
-      return position = String.format("(%s, %s)", this.x, this.y);
+        Puts("Would you like to start the game? y = affirmative");
+        Scanner inputHandler = new Scanner(System.in);
+
+        while (wantsToPlay == HandleInput(inputHandler.nextLine()))
+        {
+            HandleInput((inputHandler.nextLine()));
+        }
     }
-  }
-  
-  public static void main(String[] args)
-  {
-    // Stuff
-  }
+
+    // region Postion Handler
+
+    private static boolean HandleInput(String input)
+    {
+        if (input.equalsIgnoreCase("stop")) return false;
+
+        // Updates position and disallows player from going out of bounds
+        switch (input)
+        {
+            case "n":
+                if (++pos.y > gameBoard[4])
+                {
+                    pos.y = 2;
+                    Puts("You can't go further North!");
+                    return true;
+                }
+                break;
+            case "s":
+                if (--pos.y < gameBoard[0])
+                {
+                    pos.y = -2;
+                    Puts("You can't go any further South!");
+                    return true;
+                }
+                break;
+            case "e":
+                if (++pos.x > gameBoard[4])
+                {
+                    pos.x = 2;
+                    Puts("You can't go any further East!");
+                    return true;
+                }
+                break;
+            case "w":
+                if (++pos.x < gameBoard[0])
+                {
+                    pos.x = -2;
+                    Puts("You can't go any further West!");return true;
+
+                }
+                break;
+            default:
+                Puts("You can't go here!");
+                return true;
+
+        }
+        Puts("Your new position is " + pos.UpdateCoord());
+
+        return true;
+    }
+
+    private static void HandleCurrentPosition()
+    {
+
+    }
+
+    // endregion
+
+    private static void Puts(String output)
+    {
+        System.out.println(output);
+    }
+
 }
